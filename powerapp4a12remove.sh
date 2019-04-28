b4775254826f077f0fa26b778d35bc6f944d6bfc
Dave T <thedave1482@gmail.com>
2019-04-28 15:56:36 -0500

    Update powerapp4a12remove.sh

--- powerapp4a12remove.sh
+++ powerapp4a12remove.sh
@@ -1,4 +1,10 @@
+if [ "$(id -u)" != "0" ]; then
+echo "This install script must be run as root" 1>&2
+exit 1
+fi
 
+if [ -e /jb/bin/bash ]
+then
 	echo "Removing..."
 	rm -rf /Applications/PowerApp.app/
 	echo "This needs uicache..."
@@ -6,3 +12,4 @@
 	echo "All done!"
 	echo "PowerApp Removed!"
 	killall -9 SpringBoard
+fi
